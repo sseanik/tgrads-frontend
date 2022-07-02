d789cc6e2179837b8103c07162a0d6f8ff8a2a0f
Sean <seaniksmith@gmail.com>
2022-07-02 19:04:12 +1000

    Fixed not feeding names through getStaticProps for login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,32 @@
-import type { NextPage } from 'next';
+import type { GetStaticProps, NextPage } from 'next';
 
 import AppShell from '../components/AppShell';
+import { QUERY_ALL_NAMES } from '../graphql/queries/people';
+import client from '../lib/apollo';
+import { mapAndSortNames } from '../utils/mapAndSortNames';
 
-const Home: NextPage = () => {
+const Home: NextPage<{
+  names: string[];
+}> = ({ names }) => {
   return (
-    <AppShell>
+    <AppShell names={names}>
       <p>hello!</p>
     </AppShell>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const {
+    data: { grads },
+  } = await client.query({
+    query: QUERY_ALL_NAMES,
+  });
+
+  const names = mapAndSortNames(grads);
+
+  return {
+    props: { names: names },
+  };
 };
 
 export default Home;
--- pages/office/index.tsx
+++ pages/office/index.tsx
@@ -1,14 +1,32 @@
-import { NextPage } from 'next';
+import { GetStaticProps, NextPage } from 'next';
 
 import AppShell from '../../components/AppShell';
+import { QUERY_ALL_NAMES } from '../../graphql/queries/people';
+import client from '../../lib/apollo';
+import { mapAndSortNames } from '../../utils/mapAndSortNames';
 
-const Office: NextPage = () => {
-
+const Office: NextPage<{
+  names: string[];
+}> = ({ names }) => {
   return (
-    <AppShell>
+    <AppShell names={names}>
       <p>Office page!</p>
     </AppShell>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const {
+    data: { grads },
+  } = await client.query({
+    query: QUERY_ALL_NAMES,
+  });
+
+  const names = mapAndSortNames(grads);
+
+  return {
+    props: { names: names },
+  };
 };
 
 export default Office;
