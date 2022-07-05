7a96765433c4186678d9140182f4bc1a495e7787
Sean Smith <sean.smith@student.unsw.edu.au>
2022-07-05 17:17:58 +1000

    Changed strapi URL behaviour

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const strapiUrl = process.env.STRAPI_URL;
+const strapiUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://tgrads.vercel.app'
+    : 'http://localhost:3000';
 
 export async function signIn({ email, password }) {
   const res = await axios.post(`${strapiUrl}/api/auth/local`, {
