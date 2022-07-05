a5e306ac9d24f531c68f94fd60f0f76098a6ab9a
Sean Smith <sean.smith@student.unsw.edu.au>
2022-07-05 17:31:28 +1000

    Fixed apollo complaining about dynamic url

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,7 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
+const strapiURL = `${process.env.STRAPI_URL}/graphql`;
+
 const client = new ApolloClient({
-  uri: `${process.env.STRAPI_URL}/graphql`,
+  uri: strapiURL,
   cache: new InMemoryCache(),
   defaultOptions: {
     watchQuery: {
